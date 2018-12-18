2e371136abbeae0411fefa74a6995a9c161b7041
Jason McCormack <jason.mccormack@druva.com>
2018-12-17 22:18:51 -0800

    Add in config file and env variable changes

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,7 +1,8 @@
+const config = require('./config/config');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //tell mongoose to use native promises
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = 
 {
